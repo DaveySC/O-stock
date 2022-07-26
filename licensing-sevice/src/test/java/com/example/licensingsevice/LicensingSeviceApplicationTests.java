f82269dad224994b4621eab9584f35b6b304ec24
DaveySC <hrisanfov1@gmail.com>
2022-07-27 02:18:57 +0400

    simple first service

---
+++ licensing-sevice/src/test/java/com/example/licensingsevice/LicensingSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.licensingsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
