78dff8b0b2458300ea4a38cdcecf959d6f6f15d0
DaveySC <hrisanfov1@gmail.com>
2022-08-04 01:52:18 +0400

    added monitoring service with spring boot admin, docker and docker-compose files

---
+++ monitoring-service/src/main/java/com/example/monitoringservice/MonitoringServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.monitoringservice;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServiceApplication.class, args);
+	}
+
+}
